0af37b64e33f1c4064dbb1b7a4eaea943fa7a85c
Capeo <odd.cappelen@gmail.com>
2016-04-01 13:13:39 +0200

    Finish query.php. Now returns JSON with results

--- query.php
+++ query.php
@@ -1,50 +1,86 @@
 <?php
+	//For testing only
+	/*
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+	*/
+	
+	//Define API key
 	$api_key = "b24b21443654829ad3a9598b26e64d7ed66b10b2";
 	
+	//Retrieve keywords from through POST method
 	$keywords = $_POST['keywords'];
 	$terms = explode(" ", $keywords);
 	
+	//Add all keywords to query using conjunction
 	$query = "A[";
 	foreach($terms as $t){                 
 		$query = $query . $t . "^";
 	}
-	rtrim($query, "^");
+	$query = rtrim($query, "^");
 	$query = $query . "]";
 	
+	//Generate correct url for query
 	$url = "https://gateway-a.watsonplatform.net/calls/data/GetNews?&apikey=" . $api_key . "&outputMode=json&rank=high&dedup=1&start=now-1d&end=now&count=10&q.enriched.url.text=" . $query . "&return=enriched.url.url,enriched.url.docSentiment.score";
 	
-	$json = file_get_contents($url);
-	$arr = json_decode($json, true);
+	//Retrieve JSON using cURL
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$json = curl_exec($ch);
+	curl_close($ch);
 	
-	$results = $arr['results']['docs'];
+	$obj = json_decode($json);
+	$results = $obj->result->docs;
 	
-	$sourceVal = array();
-	
-	foreach($results as $doc => $values){
-		$docRes = $values['source']['enriched']['url'];
-		$sentiment = $docRes['docSentiment']['score'];
-		$docUrl = $docRes['url'];
+	//For each result retrieved find source name and document sentiment. Store in associative array with source name as key
+	$sources = array();
+	foreach($results as $doc){
+		$docRes = $doc->source->enriched->url;
+		$sentiment = $docRes->docSentiment->score;
+		$docUrl = $docRes->url;
 		
-		ltrim($docUrl, "http://");
-		if (strpos($docUrl, "/") !== false) {
-			$source = substr($docUrl, 0, strpos($docUrl, "/"));
+		//Parse source name
+		if(strpos($docUrl, "http://") !== false){
+			$source = ltrim($docUrl, "http://");
+			if (strpos($source, "/") !== false) {
+				$source = substr($source, 0, strpos($source, "/"));
+			}	
 		}
-		
-		if(array_key_exists($source, $sourceVal)){
-			$sourceVal[$source][0] += sentiment;
-			$sourceVal[$source][1] += 1;
+		elseif(strpos($docUrl, "https://") !== false){
+			$source = ltrim($docUrl, "https://");
+			if (strpos($source, "/") !== false) {
+				$source = substr($source, 0, strpos($source, "/"));
+			}	
 		}
 		else{
-			$sourceVal[$source] = array($sentiment, 1);
+			$source = "Unknown";
+		}
+		
+		if(array_key_exists($source, $sources)){
+			$sources[$source][0] += $sentiment;
+			$sources[$source][1] += 1;
+		}
+		else{
+			$sources[$source] = array($sentiment, 1);
 		}
 	}
 	
+	//Calculate average sentiment of each source and store in associative array. Also calculate 'global' average accross all retrieved articles 
+	$global_total = 0;
+	$global_num = 0;
 	$sentiments = array();
-	
-	foreach($sourceVal as $source){
-		$sentiments[$source] = $sourceVal[$source][0] / $sourceVal[$source][1];
+	foreach($sources as $source => $value){
+		$sentiments[$source] = $value[0] / $value[1];
+		$global_total += $value[0];
+		$global_num += $value[1];
 	}
+	$global_avg = $global_total / $global_num;
+	$sentiments["Global"] = $global_avg;
 	
+	
+	//Return JSON of associative array
 	echo json_encode($sentiments);
 	
 ?>
